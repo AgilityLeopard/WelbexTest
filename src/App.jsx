527a2831bab64cf7488d0e2d99f4b559a2e9fea0
AgilityLeopard <agilityleopard@gmail.com>
2023-03-11 22:12:34 +0300

    version 0

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Union from "./union.jsx";
+
+import "./scss/app.scss";
+
+const App = () => {
+  return (
+    <div>
+      <Union />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/union.jsx
@@ -0,0 +1,15 @@
+import Header from "./header";
+import Content from "./content";
+import Footer from "./footer";
+
+const Union = () => {
+  return (
+    <>
+      <Header />
+      <Content />
+      <Footer />
+    </>
+  );
+};
+
+export default Union;
